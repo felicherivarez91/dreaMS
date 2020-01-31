a66213b9ccbab16263c2af4edfcaa255ecd1defc
Javier Serrano <javier.serrano@healios.tech>
2020-01-31 14:31:33 +0100

    Add Welcome Activity Add Circular Std font familiy Add Text Styles ADd Button Styles

---
+++ app/src/main/java/com/healios/dreams/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,14 @@
+package com.healios.dreams.ui.welcome
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.healios.dreams.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+
+}
